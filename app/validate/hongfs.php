346f14ad89f03a454d4930437a76669521b6334a
hongfs <hong@hongfs.cn>
2024-01-21 11:35:59 +0800

    #

---
+++ app/validate/hongfs.php
@@ -0,0 +1,33 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\validate;
+
+use think\Validate;
+
+class hongfs extends Validate
+{
+    /**
+     * 定义验证规则
+     * 格式：'字段名' =>  ['规则1','规则2'...]
+     *
+     * @var array
+     */
+    protected $rule = [
+        'hongfs' => 'require',
+    ];
+
+    /**
+     * 定义错误信息
+     * 格式：'字段名.规则名' =>  '错误信息'
+     *
+     * @var array
+     */
+    protected $message = [
+        'hongfs.require' => 'hongfs为什么不写',
+    ];
+
+    protected $scene = [
+        'edit'  =>  ['hongfs'],
+    ];
+}
